e2450ae098dfa4f30a04ccc446ecf408dd75ae53
Iisakki Kulhua <iisakki@kulhua.net>
2020-10-26 18:38:29 +0200

    Heroku testi

--- src/main/java/projekti/DefaultController.java
+++ src/main/java/projekti/DefaultController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class DefaultController {
 
-    @GetMapping("/")
+    @GetMapping("dry-chamber-49238.herokuapp.com/")
     public String helloWorld(Model model) {
         return "home";
     }
