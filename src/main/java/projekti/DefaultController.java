2d110990ffbb14b500e938a4c38428e052b30d9b
Iisakki Kulhua <iisakki@kulhua.net>
2020-10-29 16:47:46 +0200

    Home simple

--- src/main/java/projekti/DefaultController.java
+++ src/main/java/projekti/DefaultController.java
@@ -1,25 +1,14 @@
 package projekti;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class DefaultController {
-
-    @GetMapping("dry-chamber-49238.herokuapp.com/")
-    public String herokuhome(Model model) {
-        return "fragments/home";
-    }
     
     @GetMapping("dry-chamber-49238.herokuapp.com/login")
     public String herokulogin() {
         return "login";
-    }
-    
-    @GetMapping("/")
-    public String home(Model model) {
-        return "fragments/demo";
     }
     
     @GetMapping("/login")
